67bfa82ae519d0b5c5e6407b76bda7554059eb27
zhengfanlei <zhengfanlei@netbank.cn>
2021-06-08 10:04:17 +0800

    record

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    tsdbUrl: 'http://10.201.5.205:4242/',
+    interval: 1000,
+}
---
+++ index.js
@@ -0,0 +1,42 @@
+const config = require('./config');
+const os = require('os-utils');
+const axios = require('axios');
+
+const getRate = () => {
+    return new Promise(resolve => {
+        os.cpuUsage((v) => {
+            resolve({
+                cpuRate: parseInt(v * 100),
+                memRate: parseInt((1 - os.freememPercentage()) * 100)
+            })
+        })
+    })
+}
+
+setInterval(async () => {
+    let data = await getRate();
+    let time = parseInt(new Date().getTime() / 1000);
+    axios({
+        method: 'post',
+        baseURL: config.tsdbUrl,
+        url: '/api/put',
+        data: [
+            {
+                metric: 'sys.cpu',
+                timestamp: time,
+                value: data.cpuRate,
+                tags: {myType: 'cpu'}
+            },
+            {
+                metric: 'sys.mem',
+                timestamp: time,
+                value: data.memRate,
+                tags: {myType: 'mem'}
+            },
+        ]
+    }).then(res => {
+        console.log('写入成功');
+    }, err => {
+        console.log('写入失败');
+    })
+}, config.interval);
